6a408b5de5f4bad30b56b1ea477a2b10d67fd2ff
Ryan Gardner <rgardner1727@gmail.com>
2025-01-30 11:43:46 -0500

    Modified the webhook route

--- backend/routes/stripeRoutes/webhookRoute.js
+++ backend/routes/stripeRoutes/webhookRoute.js
@@ -21,6 +21,19 @@ router.post('/', (req, res) => {
         return res.status(400).send('Webhook signature verification failed.');
     }
 
+    switch (event.type) {
+        case 'payment_intent.succeeded':
+            const paymentIntentSucceeded = event.data.object;
+            console.log('Payment intent succeeded', paymentIntentSucceeded);
+            break;
+        case 'payment_intent.payment_failed':
+            const paymentIntentFailed = event.data.object;
+            console.log('Payment intent failed', paymentIntentFailed);
+            break;
+        default: 
+            console.log('Unhandled event type', event.type);
+    }
+
     console.log('Webhook signature verification successful.');
     return res.status(200).send('Webhook signature verification successful.');
 })
